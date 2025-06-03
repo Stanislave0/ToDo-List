8a82a6261f8eeecf30eb45d032dc6826829841c3
Stanislave0 <stassot67@gmail.com>
2025-06-04 00:08:29 +0300

    basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Stanislave0/ToDo-List',
+  base: '/ToDo-List/',
   plugins: [react()],
 })
